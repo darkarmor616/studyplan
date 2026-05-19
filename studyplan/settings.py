e7082d474ceb9dd730ba63366b427467f29f8bfa
upscaledigital <upscale.mkt@gmail.com>
2026-05-19 09:35:47 -0300

    fix: adiciona CSRF_TRUSTED_ORIGINS para produção

--- studyplan/settings.py
+++ studyplan/settings.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-studyplan-dev-key-troque-em-producao')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+CSRF_TRUSTED_ORIGINS = ['https://' + host for host in ALLOWED_HOSTS if host != '*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
